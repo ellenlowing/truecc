e4be15f46f2f65557fa39ca7fe6e24b30b238ba0
Ellen Lo <ellen.lo@nytimes.com>
2021-01-16 23:28:52 -0500

    replace bg image with file upload

--- assets/script.js
+++ assets/script.js
@@ -121,7 +121,7 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function (e) {
-      $("#imgpreview").attr("src", e.target.result);
+      $("body").css("background", `url("${e.target.result}")`);
     };
     reader.readAsDataURL(input.files[0]); // convert to base64 string
   }
